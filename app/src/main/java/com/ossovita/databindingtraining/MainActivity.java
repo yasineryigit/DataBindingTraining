54fb99c36fc512b5097fabffc1c97ccdbc0c03b6
sancaryum <yasineryigit737@gmail.com>
2021-03-08 20:29:04 +0300

    Data Binding Event Listener is done

--- app/src/main/java/com/ossovita/databindingtraining/MainActivity.java
+++ app/src/main/java/com/ossovita/databindingtraining/MainActivity.java
@@ -3,14 +3,18 @@ package com.ossovita.databindingtraining;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ossovita.databindingtraining.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding amb;
+    private MainActivityClickHandler handlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         amb = DataBindingUtil.setContentView(this,R.layout.activity_main);
         amb.setStudent(getCurrentStudent());
 
-
+        handlers = new MainActivityClickHandler(this);
+        amb.setClickHandler(handlers);
     }
 
     private Student getCurrentStudent(){
@@ -27,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
         student.setStudentName("Mike");
         student.setStudentEmail("mike@gmail.com");
         return student;
+    }
+
+    public class MainActivityClickHandler{
+        Context context;
+
+        public MainActivityClickHandler(Context context) {
+            this.context = context;
+        }
+
+        public void onEnrollButtonClicked(){
+            Toast.makeText(context, "Enroll clicked", Toast.LENGTH_SHORT).show();
+        }
+        public void onCancelButtonClicked(){
+            Toast.makeText(context, "Cancel clicked", Toast.LENGTH_SHORT).show();
+
+        }
     }
 
 
